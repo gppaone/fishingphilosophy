dc22f00fdc21282d6b8c39fb913180dc1a3aaa44
Greg Paone <gpaone@gmail.com>
2025-08-27 15:13:50 +0000

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="bg-blue-500 text-white p-4">Welcome to SvelteKit</h1>
+<p class="text-2xl font-bold">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
