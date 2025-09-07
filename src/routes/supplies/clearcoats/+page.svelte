417cf1feb9e74037d12caa8eea8af41c440e86d2
gppaone_ncstate <gppaone@ncsu.edu>
2025-09-07 10:06:31 -0400

    Added clear coats

--- src/routes/supplies/clearcoats/+page.svelte
+++ src/routes/supplies/clearcoats/+page.svelte
@@ -1,3 +1,68 @@
 <div class="container mx-auto">
-    <h1 class="text-2xl">Clear Coats</h1>
+    <div class="p-4">
+        <h1 class="text-4xl font-bold mb-4">Top Clear Coats</h1>
+        <p>Here are my top 3 clear coats for lure building.  (See <a href="#things" class="text-blue-700 underline">Other Things</a> options below.)</p>
+        <div class="md:flex pt-4">
+            <div class="md:pr-4 pb-4 flex justify-center">
+                <a href="https://amzn.to/3JRuxzJ" target="_blank" title="Flexcoat UV Ultra V"><img src="https://m.media-amazon.com/images/I/61HtJi73TmL._AC_SL1017_.jpg" alt="Flexcoat UV Ultra V" class="w-48 max-w-none" /></a>
+            </div>
+            <div>
+                <h2 class="text-xl font-bold">Two-Part Epoxy Resins (e.g., Envirotex Lite, Flex-Coat)</h2>
+                <p>Flex-Coat is easily my most favorite.  It offers a glass-like, ultra-durable finish that resists chipping, cracking, and plasticizer damage, making it perfect for high-wear environments.</p>
+                <p class="pt-2">Amazon: <a href="https://amzn.to/3JRuxzJ" target="_blank" class="text-blue-700 underline">Flexcoat UV Ultra V</a></p>
+            </div>
+        </div>
+        <hr class="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700">
+        <div class="md:flex pt-4">
+            <div class="md:pr-4 pb-4 flex justify-center">
+                <a href="https://amzn.to/4gbXVwF" target="_blank" title="KBS DiamondFinish Clear"><img src="https://m.media-amazon.com/images/I/81rO4U-lkNL._AC_SX679_.jpg" alt="KBS Diamond Finish Clear" class="w-48 max-w-none" /></a>
+            </div>
+            <div>
+                <h2 class="text-xl font-bold">KBS Diamond Finish Clear</h2>
+                <p>Another very popular option is KBS Diamond Finish Clear Coat.  It's a moisture-cured urethane that delivers a deep, high-gloss finish, provides excellent flexibility and will never yellow or crack, making it ideal for long-lasting lure finishes—plus, it works superbly for dip-coating.</p>
+                <p class="pt-2">Amazon: <a href="https://amzn.to/4gbXVwF" target="_blank" class="text-blue-700 underline">KBS Diamond Finish Clear (32oz)</a></p>
+            </div>
+        </div>
+        <hr class="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700">
+        <div class="md:flex pt-4">
+            <div class="md:pr-4 pb-4 flex justify-center">
+                <a href="https://amzn.to/4ga2xTU" target="_blank" title="Devcon 2 Ton Epoxy"><img src="https://m.media-amazon.com/images/I/61k5YALh-mL._SL1000_.jpg" alt="Devcon 2 Ton Epoxy" class="w-48 max-w-none" /></a>
+            </div>
+            <div>
+                <h2 class="text-xl font-bold">Devcon 2 Ton Epoxy</h2>
+                <p>Highly praised for its long-term clarity and resistance to yellowing over time, making it a reliable standard for durable lure finishes.</p>
+                <p class="pt-2">Amazon: <a href="https://amzn.to/4ga2xTU" target="_blank" class="text-blue-700 underline">Devcon 2 Ton Epoxy</a></p>
+            </div>
+        </div>
+
+        <p class="pt-4 font-bold">What do else do you need?  See <a href="/supplies/accessories" class="text-blue-600 underline">Accessories</a> options</p>
+        <hr class="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700">
+        <h1 class="text-4xl font-bold mb-4 pt-4" id="things">Other Things</h1>
+        <p class="mt-4 py-8 px-4 rounded bg-amber-100">As you build and paint your lures, it’s helpful to have extra tools on hand—whether for cutting a stencil, adding an eye, or simply protecting yourself from paint fumes and vapors.</p>
+        <div class="md:flex pt-4">
+            <div class="md:pr-4 pb-4 flex justify-center">
+            <a href="https://amzn.to/4p7wN69" target="_blank" title="X-ACTO X3000 Rubber-Barrel Hobby Knife" class="w-48 flex justify-center"><img src="https://m.media-amazon.com/images/I/615vAz4R45L._AC_SL1500_.jpg" alt="X-ACTO X3000 Rubber-Barrel Hobby Knife" class="h-48 object-contain" /></a>
+            </div>
+            <div>
+                <h2 class="text-xl font-bold">X-ACTO X3000 Rubber-Barrel Hobby Knife</h2>
+                <p>A razor-sharp blade like the X-ACTO X3000 is invaluable for lure painting, allowing you to accurately cut stencils, cleanly trim troublesome paint areas, or delicately add eyes and decals with precision. Its fine #11 stainless-steel blade and textured, anti-roll handle make it easy to control for intricate detailing, while built-in blade storage keeps extra replacements close at hand.</p>
+                <p class="pt-2">Amazon: <a href="https://amzn.to/4p7wN69" target="_blank" class="text-blue-700 underline">X-ACTO X3000 Rubber-Barrel Hobby Knife</a></p>
+            </div>
+        </div>
+        <hr class="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700">
+        <div class="md:flex pt-4">
+            <div class="md:pr-4 pb-4 flex justify-center">
+                <a href="https://amzn.to/4872QNl" target="_blank" title="Respirator Reusable Half Face Cover Gas Mask with Safety Glasses Protection"><img src="https://m.media-amazon.com/images/I/61+Rq0t3NsS._SL1000_.jpg" alt="Respirator Reusable Half Face Cover Gas Mask with Safety Glasses Protection" class="w-48 max-w-none" /></a>
+            </div>
+            <div>
+                <h2 class="text-xl font-bold">Respirator Reusable Half Face Cover Gas Mask with Safety Glasses, Filters for Painting, chemical, Organic Vapor, Welding, Polishing, Woodworking and Other Work Protection</h2>
+                <p>Wearing a quality respirator like the RBLCXG Reusable Half-Face Respirator with Safety Goggles is essential when painting, as it helps protect your lungs from inhaling fumes, dust, and solvent vapors, while the integrated goggles shield your eyes from irritating overspray. The comfortable silicone seal, adjustable strap design, and replaceable filters ensure a reliable fit and long-lasting usage for safer, focused work.</p>
+                <p class="pt-2">Amazon: <a href="https://amzn.to/4872QNl" target="_blank" class="text-blue-700 underline">Respirator Reusable Half Face Cover Gas Mask with Safety Glasses Protection</a></p>
+            </div>
+        </div>
+        <div class="mt-8 text-gray-700">
+            <h1 class="font-bold">Affiliate Disclosure</h1>
+            <p class="text-sm">As an Amazon Associate I earn from qualifying purchases. Some of the links on this page are affiliate links, which means I may earn a small commission if you make a purchase through them—at no extra cost to you. This helps support the site and allows me to keep sharing lure painting resources. Thank you for your support!</p>
+        </div>
+    </div>
 </div>
