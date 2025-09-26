e7ea2ff738d37a21ca6e7ea03db6110eca2762d9
gppaone_ncstate <gppaone@ncsu.edu>
2025-09-26 12:10:30 -0400

    Change home button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <!-- src/routes/+page.svelte -->
 <script>
-  import RandomVideos from '$lib/components/RandomVideos.svelte';
+    import RandomVideos from '$lib/components/RandomVideos.svelte';
 </script>
 
 <div class="container mx-auto">
@@ -14,8 +14,18 @@
         "
     >
         <div class="absolute top-1/2 right-4 transform -translate-y-1/2 bg-[rgba(255,255,255,0.8)] p-6 rounded-lg shadow-lg max-w-sm">
-            <p class="text-gray-800 mb-4">Looking for tools to get started?</p>
-            <a href="/supplies" class="text-blue-600 hover:text-blue-800 underline text-2xl font-semibold">See Recommended Supplies</a>
+            <p class="text-gray-800 mb-4 text-xl font-bold">Looking for tools to get started?</p>
+            <a href="/supplies" class="text-white bg-blue-700 text-xl hover:bg-blue-500 px-4 py-2 rounded-lg w-fit ease duration-300 flex gap-1 items-center group">
+                <span>See Recommended Supplies</span>
+                <svg data-v-e660a7a7="" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                    aria-hidden="true" role="img"
+                    class="group-hover:translate-x-1 transition-transform ease duration-200" width="1em" height="1em"
+                    viewBox="0 0 256 256">
+                    <path fill="currentColor"
+                        d="m221.66 133.66l-72 72a8 8 0 0 1-11.32-11.32L196.69 136H40a8 8 0 0 1 0-16h156.69l-58.35-58.34a8 8 0 0 1 11.32-11.32l72 72a8 8 0 0 1 0 11.32Z">
+                    </path>
+                </svg>
+            </a>
         </div>
     </div>
     <div class="p-4">
